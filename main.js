86df790d63c8d588e0a04dff7b9543c8d383e3de
sgoshaa <sgoshaa@yandex.ru>
2019-09-24 21:32:21 +0500

    add enemy

--- main.js
+++ main.js
@@ -22,20 +22,35 @@ const keys={
 const setting = {
     start:false,
     score:0,
-    speed:3
+    speed:3,
+    traffic:3
 };
+
+function getQuantityElements(heightElement){
+    return document.documentElement.clientHeight / heightElement + 1;
+}
 
 function startGame(){ 
     start.classList.add('hide');
 
-    for(let i = 0;i<20;i++){
+    for(let i = 0;i<getQuantityElements(100);i++){
         const line = document.createElement('div');
         line.classList.add('line');
         line.style.top = (i*100)+'px';
         line.y = i*100;
         gameArea.appendChild(line);
-
     }
+    for (let i=0;i<getQuantityElements(100*setting.traffic);i++){
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y=-100 * setting.traffic * i + 1;
+        enemy.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50))+'px';
+        enemy.style.background = ' transparent url(./image/enemy.png) center / cover no-repeat';
+        enemy.style.top=enemy.y+'px';
+        gameArea.appendChild(enemy);
+    }
+
+
     setting.start = true;
     gameArea.appendChild(car);
     setting.x=car.offsetLeft;
@@ -50,12 +65,24 @@ function moveRoad(){
         if(line.y>document.documentElement.clientHeight){
             line.y=-100;
         }
-    })
+    });
 
+}
+function moveEnemy(){
+    let enemys = document.querySelectorAll('.enemy');
+    enemys.forEach(function(item){
+        item.y+=setting.speed/2;
+        item.style.top=item.y+'px';
+        if(item.y>document.documentElement.clientHeight){
+            item.y=-100*setting.traffic;
+            item.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50))+'px';
+        }
+    });
 }
 
 function playGame(){
    moveRoad();
+   moveEnemy();
     if(setting.start){
         if(keys.ArrowLeft && setting.x > 0){
             setting.x-=setting.speed;
