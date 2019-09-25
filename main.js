19439288057e417610eeffc405a06cf6feb127e2
sgoshaa <sgoshaa@yandex.ru>
2019-09-25 20:42:01 +0500

    добавлены ДТП и счет игры

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ function moveRoad(){
     lines.forEach(function(line){
         line.y+=setting.speed;
         line.style.top= line.y+'px';
-        if(line.y>document.documentElement.clientHeight){
+        if(line.y >= document.documentElement.clientHeight){
             line.y=-100;
         }
     });
@@ -71,9 +71,18 @@ function moveRoad(){
 function moveEnemy(){
     let enemys = document.querySelectorAll('.enemy');
     enemys.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+        if(carRect.top<=enemyRect.bottom &&
+          carRect.right>=enemyRect.left && 
+          carRect.left<=enemyRect.right &&
+          carRect.bottom>=enemyRect.top){
+            setting.start = false;
+        }
+
         item.y+=setting.speed/2;
         item.style.top=item.y+'px';
-        if(item.y>document.documentElement.clientHeight){
+        if(item.y >= document.documentElement.clientHeight){
             item.y=-100*setting.traffic;
             item.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50))+'px';
         }
